ee5c2849e14f4b69a756c4aab22b89c829c5852d
letrongkhanh <144077765+letrongkhanh@users.noreply.github.com>
2023-12-20 15:09:59 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,20 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 100;
+		int sum = 0;
+		int i = 1;
+
+		while (i <= n) {
+			sum += i;
+			i++;
+		}
+
+		System.out.println("Tong cac so tu 1 den 100: " + sum);
+
+	}
+
+}
